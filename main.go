9bc0dc8030dc57886d8ad0a758680ec0edfbc94a
vikramaditya pandey <53808060+vikkooo013@users.noreply.github.com>
2021-04-03 08:30:47 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
